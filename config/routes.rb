4062d0d17caeb4bc0535476b1af3229ce11bca33
EC2 Default User <ec2-user@ip-172-31-2-215.ec2.internal>
2021-12-30 14:19:18 +0000

    [start] EX3 コメント機能実装　レイアウトまだ

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books,only: [:new,:create,:index,:show,:destroy,:edit,:update ]
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books, only: [:new,:create,:index,:show,:destroy,:edit,:update ] do
+    resources :book_comments, only: [:create, :destroy]
+  end
 
 
 end
