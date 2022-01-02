382c577b06d4f7639eebd8340110ffcf3e65e1db
EC2 Default User <ec2-user@ip-172-31-2-215.ec2.internal>
2022-01-02 06:36:12 +0000

    [finish] EX3　要件クリア　コード見直し必要

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,11 +2,10 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book_comment_params
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    redirect_to request.referer
   end
 
   def destroy
