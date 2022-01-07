9a23b81e32ffd79591051822ca10a7a8ae04f5eb
EC2 Default User <ec2-user@ip-172-31-2-215.ec2.internal>
2022-01-07 09:46:35 +0000

    [add] 非同期いいね機能完了　次はコメント機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,20 @@
 class FavoritesController < ApplicationController
 
+  #エラーで非同期通信が出来なかったのは定義出来ていなかった。
+  #失敗した時はデベロッパーからネットワークでエラーを確認。
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: @book.id)
+    @favorite.save
+    #redirect_to book_path(book)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    @favorite.destroy
+    #redirect_to book_path(book)
   end
 
 end
